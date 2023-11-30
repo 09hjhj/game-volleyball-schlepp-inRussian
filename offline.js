7a091a3a69e6cab07b62bcd9ad90b7a402314c45
floppa728374 <150988840+09hjhj@users.noreply.github.com>
2023-11-30 10:19:50 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1701321514,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Спрайт-sheet0.png",
+		"images/подушкаш-sheet0.png",
+		"images/арбузш-sheet0.png",
+		"images/синий-sheet0.png",
+		"images/рамка-sheet0.png",
+		"images/розовый-sheet0.png",
+		"images/серидина-sheet0.png",
+		"images/мяч-sheet0.png",
+		"images/победаподушки-sheet0.png",
+		"images/победаарбуза-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
